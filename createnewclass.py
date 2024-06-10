09d04ff117dcc576382cbe9886ac6a7b7b111374
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2024-06-10 00:02:41 -0700

    windows compat

--- createnewclass.py
+++ createnewclass.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from datetime import date, datetime
 import subprocess
+import platform
 
 your_schedule = "quarter" # replace with "semester" if you're on a semester system
 github_username = "eeganr"
@@ -10,6 +11,7 @@ github_actions = True # Change to false if you wanna do this manually
 # ----------------
 
 notes_folder = os.path.dirname(os.getcwd())
+sys = platform.system() == 'Windows'
 
 def get_suggested_name():
     Y = 2024 # dummy leap year to allow input X-02-29 (leap day)
@@ -72,9 +74,11 @@ if not dir_check:
         s = s.replace('name', parent)
         f.write(s)
     command = 'git init; git add .; git commit -m "initialized"; git branch -M main;'
+    if sys: command = command.replace(';', ' &')
     subprocess.run(command, shell=True, cwd=folder)
     if github_actions:
         command = f'gh repo create {parent} --private; git remote add origin https://github.com/{github_username}/{parent}.git; git push -u origin main'
+        if sys: command = command.replace(';', ' &')
         subprocess.run(command, shell=True, cwd=folder)
     
 else:
@@ -114,6 +118,7 @@ replace_ids(f"{folder}/{class_id}/{short_id}_NOTES_TEMPLATE/{short_id}_MM-DD.tex
 replace_ids(f"{folder}/{class_id}/{short_id}_HW_TEMPLATE/{short_id}_HW_X.tex")
 
 command = f'git init; git add .; git commit -m "{class_id} added"'
+if sys: command = command.replace(';', ' &')
 subprocess.run(command, shell=True, cwd=folder)
 
 if github_actions:
