f0e77062b0c752e416f10fde531f410c6924510f
Eegan Ram <eeganr@Eegans-MBP.attlocal.net>
2025-07-07 18:57:42 -0700

    Update commit scripts and style

--- commit.bat
+++ commit.bat
@@ -1,3 +1,16 @@
+@echo off
+setlocal
+
+:: Prompt for commit message
+set /p commit_message=Please enter your commit message: 
+
+:: Stage all changes
 git add .
-git commit -m "notes and hw"
+
+:: Commit with the provided message
+git commit -m "%commit_message%"
+
+:: Push to the selected branch
 git push -u origin main
+
+endlocal
--- commit.sh
+++ commit.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+echo "Please enter your commit message:"
+read commit_message
+
 git add .
-git commit -m "notes and hw"
+git commit -m "$commit_message"
+
 git push -u origin main
